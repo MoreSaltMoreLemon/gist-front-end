c4865fc38f7f7905680f071f705963da4aa6cea2
Ezra Schwepker <ezraschwepker@gmail.com>
2019-05-08 14:07:41 -0700

    netflify build issues

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
-const BASE_URL = 'http://localhost:3000'
-const API_URL = `${BASE_URL}/api/v1`
+const DEV_URL = "http://localhost:3000";
+const DEPLOY_URL = "https://gist-recipes.herokuapp.com/";
+const BASE_URL = DEPLOY_URL;
+const API_URL = `${BASE_URL}/api/v1`;
 
-const USERS_URL = `${API_URL}/users`
-const LOGIN_URL = `${API_URL}/login`
-const LOGOUT_URL = `${API_URL}/logout`
+const USERS_URL = `${API_URL}/users`;
+const LOGIN_URL = `${API_URL}/login`;
+const LOGOUT_URL = `${API_URL}/logout`;
 
-const RECIPES_URL = `${API_URL}/recipes`
-const RECIPE_STEPS_URL = `${API_URL}/recipe_steps`
-const STEP_INGREDIENTS_URL = `${API_URL}/step_ingredients`
-const STEP_SUB_RECIPES_URL = `${API_URL}/step_sub_recipes`
+const RECIPES_URL = `${API_URL}/recipes`;
+const RECIPE_STEPS_URL = `${API_URL}/recipe_steps`;
+const STEP_INGREDIENTS_URL = `${API_URL}/step_ingredients`;
+const STEP_SUB_RECIPES_URL = `${API_URL}/step_sub_recipes`;
 
 export {
   BASE_URL,
@@ -20,4 +22,4 @@ export {
   RECIPE_STEPS_URL,
   STEP_INGREDIENTS_URL,
   STEP_SUB_RECIPES_URL
-}
+};
