7b21c39e41cb7ca6d377f2357d04654574b8e4e3
Ezra Schwepker <ezraschwepker@gmail.com>
2019-05-08 16:08:52 -0700

    beginning work on private recipe protection

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const DEV_URL = "http://localhost:3000";
 const DEPLOY_URL = "https://gist-recipes.herokuapp.com/";
-const BASE_URL = DEPLOY_URL;
+const BASE_URL = DEV_URL;
 const API_URL = `${BASE_URL}/api/v1`;
 
 const USERS_URL = `${API_URL}/users`;
